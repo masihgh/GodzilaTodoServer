fc28d36f98e0463e2cfd5430a54f0c6539a44c97
masihgh <masihprogrammer@gmail.com>
2023-03-22 20:08:36 +0330

    npm i + create router for members :(

---
+++ routes/member.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+// router.get('/list', getAllTodos)
+// router.get('/:id', getTodo)
+// router.post('/', createTodo)
+// router.patch('/:id', updateTodo)
+// router.delete('/:id', deleteTodo)
+
+
+module.exports = router
