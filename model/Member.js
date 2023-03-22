67e9d79237ec3b2b623876a9dd3ea761e48b6050
masihgh <masihprogrammer@gmail.com>
2023-03-22 20:32:43 +0330

    Add unique for some Fields

--- model/Member.js
+++ model/Member.js
@@ -1,41 +1,44 @@
 const mongoose = require("mongoose");
 
 const MemberSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  github: {
-    type: String,
-    required: true,
-  },
-  linkedin: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  language: {
-    type: String,
-    required: true,
-    default: 'en',
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  skills: {
-    type: Array,
-    required: true,
-    default: [],
-  },  
-  is_admin: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    github: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    linkedin: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    language: {
+        type: String,
+        required: true,
+        default: 'en',
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    skills: {
+        type: Array,
+        required: true,
+        default: [],
+    },
+    is_admin: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
 });
 
 module.exports = mongoose.model("Member", MemberSchema);
