4b7cc899531045a2bb1bb067bfd5ee022d2d71ec
masihgh <masihprogrammer@gmail.com>
2023-03-23 16:29:12 +0330

    change middleware json & add fileUpload Middleware

--- server.js
+++ server.js
@@ -3,21 +3,29 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./db/connect')
 const todo = require('./routes/todo')
+const member = require('./routes/member')
 //Define
 const app = express()
 const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
+var bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 // Configs
 dotenv.config()
 
 //Middlewares
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({
+  createParentPath: true,
+}));
 
 //Routes
 app.use('/todo', todo)
+app.use('/member', member)
 app.get('/', (req, res) => {
   res.json({
     'status':'success',
